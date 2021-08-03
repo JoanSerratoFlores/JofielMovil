d41f7e00bc1b821c656e3535b2c37c2f8aeba222
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2021-08-03 00:21:02 -0500

    arreglos en el header

--- src/app/curriculum-vitae/components/header/header.component.ts
+++ src/app/curriculum-vitae/components/header/header.component.ts
@@ -9,37 +9,6 @@ export class HeaderComponent implements OnInit {
   public logoBlanco = "../../assets/iconos/jofiel_blanco-06.svg"
 
 
-  public headerButtons = 
-  {
-    data:[
-      {
-        img: "../../../assets/iconos/inicio.png",
-        label:"Inicio"
-      },
-      {
-        img: "../../../assets/iconos/cuestionarios.png",
-        label:"Cuestionario"
-      },
-      {
-        img: "../../assets/iconos/suscribirse.png",
-        label:"Suscribirse"
-      },
-      {
-        img: "../../../assets/iconos/sonido.svg",
-        label:"Audio-Artículos"
-      },
-      {
-        img: "../../../assets/iconos/mensaje.png",
-        label:"Monetizar"
-      },
-      {
-        img: "../../../assets/iconos/noti.png",
-        label:"Notificaciones"
-      }
-
-    ]
-    
-  }
   constructor() { }
 
   ngOnInit() {}
